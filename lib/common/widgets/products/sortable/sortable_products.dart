d46d9740f45b66a806f6e67425dc32a43c2e8d08
Mehmet veysi özek <114666552+mehmetozek01@users.noreply.github.com>
2026-09-25 03:19:24 +0300

    Remove unused imports in sortable_products.dart
    Removed unused imports from sortable_products.dart.

--- lib/common/widgets/products/sortable/sortable_products.dart
+++ lib/common/widgets/products/sortable/sortable_products.dart
@@ -1,8 +1,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:t_store/common/widgets/layouts/grid_layout.dart';
-import 'package:t_store/common/widgets/products/product_cards/product_card_vertical.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 
 class TSortableProducts extends StatelessWidget {
